e1d370255154895bad1bda20cdd526e1d2a2503d
Ernela <fenixoffc@gmail.com>
2025-12-26 14:15:54 +0200

    small edits

--- src/config.ts
+++ src/config.ts
@@ -2,18 +2,18 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://Ernous.github.io/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
-  ogImage: "astropaper-og.jpg",
+  author: "Ernela",
+  desc: "Ernela bio",
+  title: "Ernela",
+  ogImage: "",
   lightAndDarkMode: true,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  langTag: ["en-EN"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+  lang: "ru", // html lang code. Set this empty and default will be "en"
+  langTag: ["ru-RU"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
 export const LOGO_IMAGE = {
@@ -26,45 +26,39 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/Ernous",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
   {
-    name: "Facebook",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Facebook`,
+    name: "GitLab",
+    href: "https://gitlab.com/Ernous",
+    linkTitle: `${SITE.title} on GitLab`,
     active: true,
   },
   {
-    name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Instagram`,
+    name: "Telegram",
+    href: "https://t.me/not_gophers",
+    linkTitle: `${SITE.title} on Telegram`,
     active: true,
   },
   {
-    name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on LinkedIn`,
+    name: "Mastodon",
+    href: "https://social.sophron.ru/@sovonegr",
+    linkTitle: `${SITE.title} on Mastodon`,
     active: true,
   },
   {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    active: false,
+    name: "RuTracker",
+    href: "https://rutracker.org/forum/profile.php?mode=viewprofile&u=51945983",
+    linkTitle: `${SITE.title} on RuTracker`,
+    active: true,
   },
   {
-    name: "Twitter",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitter`,
-    active: false,
-  },
-  {
-    name: "Twitch",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitch`,
-    active: false,
+    name: "NNM Club",
+    href: "https://nnmclub.to/forum/profile.php?mode=viewprofile&u=14390161",
+    linkTitle: `${SITE.title} on NNM Club`,
+    active: true,
   },
   {
     name: "YouTube",
